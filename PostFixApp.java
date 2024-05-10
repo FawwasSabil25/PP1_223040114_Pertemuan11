e565bb4568850fe7ced2315b4b9d714914ae2a22
FawwasSabil25 <fawwas2573@gmail.com>
2024-05-10 23:30:50 +0700

    mengerjakan latihan pertemuan 11

---
+++ PostFixApp.java
@@ -0,0 +1,27 @@
+package Pertemuan11;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class PostFixApp {
+    public static void main(String[] args) throws IOException {
+        String input;
+        int output;
+
+        while(true){
+            System.out.println("enter Postfix : ");
+            System.out.flush();
+            input = getString();
+            if(input.equals(" ")) break;
+
+            ParsePost aParser = new ParsePost(input);
+            output = aParser.doParse();
+            System.out.println("Hasil operasi : "+ output);
+        }
+    }
+
+    public static String getString() throws IOException{
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        return s;
+    }
+}
